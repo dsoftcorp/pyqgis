58637e872d530f6d8c61ad742cbeeb15686322ea
Dave Silva <david.silvaw@gmail.com>
2020-06-08 14:23:25 -0500

    PLugin Crs
    Last plugin

--- materiales/plugin/__init__.py
+++ materiales/plugin/__init__.py
@@ -1,58 +1,40 @@
-import os
-import re
-from qgis.PyQt import uic
+# -----------------------------------------------------------
+# Copyright (C) 2015 Martin Dobias
+# -----------------------------------------------------------
+# Licensed under the terms of GNU GPL 2
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+# ---------------------------------------------------------------------
+
 from qgis.PyQt.QtGui import *
 from qgis.PyQt.QtCore import *
+from qgis.gui import *
+
 
 def classFactory(iface):
-    return RegexPlugin(iface)
-
-def selectByRegex(layer, field, regex):
-    exp = re.compile(regex)
-    features = layer.getFeatures()
-    ids = []
-    for feature in features:
-        if exp.search(feature[field]):
-            ids.append(feature.id())
-    layer.selectByIds(ids)
+    return ProjectCrs(iface)
 
 
-class RegexPlugin:
+def setProjectCrs():
+    dialog = QgsProjectionSelectionDialog()
+    if dialog.exec_():
+        crs = dialog.crs()
+        QgsProject.instance().setCrs(crs)
+
+
+class ProjectCrs:
     def __init__(self, iface):
         self.iface = iface
+        iface.newProjectCreated.connect(setProjectCrs())
 
     def initGui(self):
-        self.action = QAction(u'Regex', self.iface.mainWindow())
-        self.action.triggered.connect(self.run)
-        self.iface.addToolBarIcon(self.action)
+        pass
 
     def unload(self):
-        self.iface.removeToolBarIcon(self.action)
-        del self.action
+        self.iface.newProjectCreated.disconnect(setProjectCrs())
 
     def run(self):
-        dialog = RegexDialog()
-        dialog.exec_()
-        if dialog.layer:
-            selectByRegex(dialog.layer, dialog.field, dialog.regex)
-            
-WIDGET, BASE = uic.loadUiType(
-    os.path.join(os.path.dirname(__file__), 'plugin.ui'))
-
-class RegexDialog(BASE, WIDGET):
-
-    def __init__(self):
-        super(RegexDialog, self).__init__(None)
-        self.layer = None
-        self.setupUi(self)
-        self.layerCombo.layerChanged.connect(layerChanged)
-        self.buttonSelect.clicked.connect(self.selectClicked)
-
-    def layerChanged(self, layer):
-        self.fieldCombo.setLayer(layer)
-
-    def selectClicked(self):
-        layer = self.layerCombo.currentLayer()
-        field = self.fieldCombo.currentField()
-        expression = self.textExpression.text()
-        selectByRegex(layer, field, expression)
+        pass
